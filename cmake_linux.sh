8b460372458e016eeb21a5a2593cbc0222a45652
Krispy <nikodash2020@hotmail.com>
2026-02-22 17:02:25 -0500

    Improved compilation workflow using visual studio code

--- cmake_linux.sh
+++ cmake_linux.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-BUILD_DIR=build/gcc/debug
-DIRECTORY="gcc"
+BUILD_DIR=build/ninja
 BUILDTYPE="debug"
 CMAKE_BUILD_TYPE="Debug"
-CMAKE_GENERATOR="Sublime Text 2 - Unix Makefiles"
+CMAKE_GENERATOR="Ninja"
 
 if [ "$#" -ne "2" ]; then
   echo "Usage cmake_linux.sh <gcc|clang|clang-libc++> <debug|release|reldeb>"
@@ -16,20 +15,17 @@ if [ "$#" -ge "1" ]; then
   if [ "$1" = "gcc" ]; then
     export CXX="g++"
     export CC="gcc"
-    DIRECTORY="gcc"
   fi
 
   if [ "$1" = "clang" ]; then
     export CXX="clang++"
     export CC="clang"   
-    DIRECTORY="clang"
     CMAKE_CXX_FLAGS="-stdlib=libstdc++"
   fi 
 
   if [ "$1" = "clang-libc++" ]; then
     export CXX="clang++"
     export CC="clang"   
-    DIRECTORY="clang-libc++"
     CMAKE_CXX_FLAGS="-stdlib=libc++"
   fi  
 fi
@@ -52,9 +48,7 @@ if [ "$#" -ge "2" ]; then
   fi    
 fi
 
-BUILDDIR=build/$DIRECTORY/$BUILDTYPE
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
 
-mkdir -p $BUILDDIR
-cd $BUILDDIR
-
-cmake -G"$CMAKE_GENERATOR" -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS" ../../../neo
+cmake -G"$CMAKE_GENERATOR" -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS" -DFORCE_COLORED_OUTPUT=ON ../../neo
